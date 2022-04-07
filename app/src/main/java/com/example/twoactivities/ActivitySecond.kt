d6ee7ac08b9d7af054babd442cc5679ea2fa7233
VictoriaRyd <victoriarydina@gmail.com>
2022-04-07 11:56:40 +0300

    Created interaction between two activities

--- app/src/main/java/com/example/twoactivities/ActivitySecond.kt
+++ app/src/main/java/com/example/twoactivities/ActivitySecond.kt
@@ -1,20 +1,34 @@
 package com.example.twoactivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivitySecond : AppCompatActivity() {
 
     val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+    val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
+    private var mReply: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        mReply = findViewById(R.id.editText_second)
 
         val intent = getIntent()
         val message = intent.getStringExtra(EXTRA_MESSAGE)
         val textView = findViewById<TextView>(R.id.text_message)
         textView.text = message
+    }
+
+    fun returnReply(view: View) {
+        val reply = mReply!!.text.toString()
+        val replyIntent = Intent(this, MainActivity::class.java)
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
     }
 }
--- app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.twoactivities
 
+import android.R.attr
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    val TEXT_REQUEST = 1
+    private var mReplyHeadTextView: TextView? = null
+    private var mReplyTextView: TextView? = null
+
     val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+    val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
     private var mMessageEditText: EditText? = null
     private val LOG_TAG = MainActivity::class.java.simpleName
 
@@ -18,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mMessageEditText = findViewById(R.id.editText_main)
+
+        mReplyHeadTextView = findViewById(R.id.text_header_reply)
+        mReplyTextView = findViewById(R.id.text_message_reply)
     }
 
     fun launchSecondActivity(view: View) {
@@ -25,6 +35,20 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, ActivitySecond::class.java)
         val message = mMessageEditText!!.text.toString()
         intent.putExtra(EXTRA_MESSAGE, message)
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST && resultCode == RESULT_OK) {
+            val reply: String = data?.getStringExtra(EXTRA_REPLY).toString()
+            mReplyHeadTextView!!.visibility = View.VISIBLE
+            mReplyTextView!!.text = reply
+            mReplyTextView!!.visibility = View.VISIBLE
+        }
     }
 }
